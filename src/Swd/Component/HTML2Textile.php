0f150d680fc693407af4a02d5868af95bfb0ae7a
Sawered <skoryukin@medee.ru>
2012-07-03 16:53:46 +0400

    Начало разработки конвертера html->textile

--- src/Swd/Component/HTML2Textile.php
+++ src/Swd/Component/HTML2Textile.php
@@ -11,7 +11,37 @@ use DOMDocument,DOMXPath;
 **/
 class  HTML2Textile
 {
+    private  $doc_template = "<html><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" /><body>%s</body></html>";
     public function __construct()
     {
+    }
+
+    public function transform($html)
+    {
+        $doc = $this->createDom($html);
+        $root = $this->getDomRoot($doc);
+
+        //TODO start from here
+    }
+
+    protected function createDom($html)
+    {
+        $html = sprintf($this->doc_template,$html);
+        $doc =  new DOMDocument("1.0","UTF-8");
+        $doc->preserveWhiteSpace = false;
+
+        @$doc->loadHTML($html);
+        $doc->normalizeDocument();
+        return $doc;
+    }
+
+    protected function getDomRoot($doc)
+    {
+        $xpath = new DOMXPath($doc);
+        $roots = $xpath->query("/html/body[1]");
+
+        $root = ($roots->item($roots->length -1));
+
+        return $root;
     }
 }
