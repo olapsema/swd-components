5dce802d5ec4f35a0b92458959c14a57f6a99e18
Sawered <skoryukin@medee.ru>
2012-07-17 21:52:27 +0400

    Добавлен класс нарезки html и перекодирования сущностей

---
+++ src/Swd/Component/HTML/HTMLEntitiesResolver.php
@@ -0,0 +1,324 @@
+<?php
+
+namespace Swd\Component\HTML;
+
+
+
+class HTMLEntitiesResolver
+{
+    static $ent_table;
+    static $default_replace = array("common","math");
+
+
+    /**
+     * Превращает UTF-8 символы  в html сущности
+     *
+     * @return void
+     * @author skoryukin
+     **/
+    static public function encode($text,$tables = false)
+    {
+        self::init();
+        if($tables == false){
+            $tables = self::$default_replace;
+        }
+
+        $from = $to = array();
+        foreach($tables as $tab){
+            $from = array_merge($from ,self::$ent_table["utf8_codes"][$tab]);
+            $to = array_merge($to ,self::$ent_table["ent"][$tab]);
+        }
+
+        return str_replace($from,$to,$text);
+    }
+
+    static protected function init()
+    {
+        static $inited;
+
+        if($inited) return;
+
+        $html = array(
+            "&quot;",
+            "&amp;",
+            "&apos;",
+            "&lt;",
+            "&gt;",
+        );
+        $common = array(
+            "&nbsp;",
+            "&iexcl;",
+            "&cent;",
+            "&pound;",
+            "&curren;",
+            "&yen;",
+            "&brvbar;",
+            "&sect;",
+            "&uml;",
+            "&copy;",
+            "&ordf;",
+            "&laquo;",
+            "&not;",
+            "&shy;",
+            "&reg;",
+            "&macr;",
+            "&deg;",
+            "&plusmn;",
+            "&sup2;",
+            "&sup3;",
+            "&acute;",
+            "&micro;",
+            "&para;",
+            "&middot;",
+            "&cedil;",
+            "&sup1;",
+            "&ordm;",
+            "&raquo;",
+            "&frac14;",
+            "&frac12;",
+            "&frac34;",
+
+            "&times;",
+            "&circ;",
+            "&tilde;",
+            "&trade;",
+
+            "&ndash;",
+            "&mdash;",
+            "&lsquo;",
+            "&rsquo;",
+            "&sbquo;",
+            "&ldquo;",
+            "&rdquo;",
+            "&bdquo;",
+            "&bull;",
+            "&hellip;",
+            "&permil;",
+            "&prime;",
+            "&Prime;",
+            "&lsaquo;",
+            "&rsaquo;",
+            "&oline;",
+            "&frasl;",
+            "&euro;",
+            "&yen;",
+        );
+
+        $latin = array(
+            "&iquest;",
+            "&Agrave;",
+            "&Aacute;",
+            "&Acirc;",
+            "&Atilde;",
+            "&Auml;",
+            "&Aring;",
+            "&AElig;",
+            "&Ccedil;",
+            "&Egrave;",
+            "&Eacute;",
+            "&Ecirc;",
+            "&Euml;",
+            "&Igrave;",
+            "&Iacute;",
+            "&Icirc;",
+            "&Iuml;",
+            "&ETH;",
+            "&Ntilde;",
+            "&Ograve;",
+            "&Oacute;",
+            "&Ocirc;",
+            "&Otilde;",
+            "&Ouml;",
+            "&Oslash;",
+            "&Ugrave;",
+            "&Uacute;",
+            "&Ucirc;",
+            "&Uuml;",
+            "&Yacute;",
+            "&THORN;",
+            "&szlig;",
+            "&agrave;",
+            "&aacute;",
+            "&acirc;",
+            "&atilde;",
+            "&auml;",
+            "&aring;",
+            "&aelig;",
+            "&ccedil;",
+            "&egrave;",
+            "&eacute;",
+            "&ecirc;",
+            "&euml;",
+            "&igrave;",
+            "&iacute;",
+            "&icirc;",
+            "&iuml;",
+            "&eth;",
+            "&ntilde;",
+            "&ograve;",
+            "&oacute;",
+            "&ocirc;",
+            "&otilde;",
+            "&ouml;",
+            "&divide;",
+            "&oslash;",
+            "&ugrave;",
+            "&uacute;",
+            "&ucirc;",
+            "&uuml;",
+            "&yacute;",
+            "&thorn;",
+            "&yuml;",
+            "&OElig;",
+            "&oelig;",
+            "&Scaron;",
+            "&scaron;",
+            "&Yuml;",
+            "&fnof;",
+        );
+
+        $greek = array(
+            "&Alpha;",
+            "&Beta;",
+            "&Gamma;",
+            "&Delta;",
+            "&Epsilon;",
+            "&Zeta;",
+            "&Eta;",
+            "&Theta;",
+            "&Iota;",
+            "&Kappa;",
+            "&Lambda;",
+            "&Mu;",
+            "&Nu;",
+            "&Xi;",
+            "&Omicron;",
+            "&Pi;",
+            "&Rho;",
+            "&Sigma;",
+            "&Tau;",
+            "&Upsilon;",
+            "&Phi;",
+            "&Chi;",
+            "&Psi;",
+            "&Omega;",
+            "&alpha;",
+            "&beta;",
+            "&gamma;",
+            "&delta;",
+            "&epsilon;",
+            "&zeta;",
+            "&eta;",
+            "&theta;",
+            "&iota;",
+            "&kappa;",
+            "&lambda;",
+            "&mu;",
+            "&nu;",
+            "&xi;",
+            "&omicron;",
+            "&pi;",
+            "&rho;",
+            "&sigmaf;",
+            "&sigma;",
+            "&tau;",
+            "&upsilon;",
+            "&phi;",
+            "&chi;",
+            "&psi;",
+            "&omega;",
+            "&thetasym;",
+            "&upsih;",
+        );
+
+        $pseudo = array(
+
+            "&larr;",
+            "&uarr;",
+            "&rarr;",
+            "&darr;",
+            "&harr;",
+            "&crarr;",
+            "&lArr;",
+            "&uArr;",
+            "&rArr;",
+            "&dArr;",
+            "&hArr;",
+            "&spades;",
+            "&clubs;",
+            "&hearts;",
+            "&diams;",
+            "&dagger;",
+            "&Dagger;",
+        );
+
+        $math = array(
+
+            "&forall;",
+            "&part;",
+            "&exist;",
+            "&empty;",
+            "&nabla;",
+            "&isin;",
+            "&notin;",
+            "&ni;",
+            "&prod;",
+            "&sum;",
+            "&minus;",
+            "&lowast;",
+            "&radic;",
+            "&prop;",
+            "&infin;",
+            "&ang;",
+            "&and;",
+            "&or;",
+            "&cap;",
+            "&cup;",
+            "&int;",
+            "&there4;",
+            "&sim;",
+            "&cong;",
+            "&asymp;",
+            "&ne;",
+            "&equiv;",
+            "&le;",
+            "&ge;",
+            "&sub;",
+            "&sup;",
+            "&nsub;",
+            "&sube;",
+            "&supe;",
+            "&oplus;",
+            "&otimes;",
+            "&perp;",
+            "&sdot;",
+            "&lceil;",
+            "&rceil;",
+            "&lfloor;",
+            "&rfloor;",
+            "&lang;",
+            "&rang;",
+            "&loz;",
+        );
+
+        $entities = array(
+            "html"  => $html,
+            "common"=> $common,
+            "greek" => $greek,
+            "latin" => $latin,
+            "math"  => $math,
+        );
+
+        $utf8_codes = array();
+        foreach($entities as $ent_tab => $ent_list){
+            foreach($ent_list as $symbol){
+                $utf8_codes[$ent_tab][] = mb_convert_encoding($symbol,"UTF-8","HTML-ENTITIES");
+            }
+        }
+        self::$ent_table = array(
+            "ent"=> $entities,
+            "utf8_codes" => $utf8_codes,
+        );
+        $inited = true;
+    }
+}
---
+++ src/Swd/Component/HTML/HTMLToolset.php
@@ -0,0 +1,225 @@
+<?php
+
+namespace Swd\Component\HTML;
+
+use DOMDocument,DOMXPath;
+
+class HTMLToolset
+{
+
+    const SUBST_NOMOD = 0;
+    const SUBST_START = 1;
+    const SUBST_END   = 2;
+    const SUBST_PRESERVE = 4;
+
+    protected $substr_ignore_tags = array("img");
+
+    public function __construct()
+    {
+        $this->substring_pos = 0;
+    }
+
+    public function shrink($html,$length)
+    {
+        return $this->substring($html,0,$length);
+    }
+
+    public function substring($html,$from,$length=false)
+    {
+        $doc = $this->getDocument($html);
+        $root = $this->getRootNode($doc);
+
+        $this->substringNode($root,$from,$length,false);
+        return $this->getDocumentHTML($doc);
+    }
+
+    public function strlength($html)
+    {
+
+    }
+
+    private $substring_pos;
+
+    protected function substNode($node,$start,$end,$callbacks = false)
+    {
+        $this->substring_pos = 0;
+        return $this->substringNode($node,$start,$end,$callbacks);
+    }
+
+    private function substringNode($node,$start,$end,$callbacks = false)
+    {
+
+        if(in_array($node->nodeName,$this->substr_ignore_tags)){
+            //обрабатываем  и для элемента создаем соответствующий тег
+            return self::SUBST_PRESERVE;
+        }
+
+
+        $result = self::SUBST_NOMOD;
+
+        $length = $this->strlenNode($node);
+
+        //$cur_pos = $pos;
+        $cur_pos = $this->substring_pos;
+        if( $start >= $cur_pos  //not started
+            && $start <= ($cur_pos + $length ) //have start point
+        ){
+            $result = $result |  self::SUBST_START;
+            if($start > $cur_pos){
+                //cut top of text
+                if($node->nodeType == XML_TEXT_NODE){
+                    $node_text = trim($node->wholeText);
+                    if(!empty($node_text)){
+                        $node_text = mb_substr($node_text,($start-$cur_pos));
+                        $node->replaceData(0,$node->length,$node_text);
+                    }
+                }
+            }
+        }
+
+        if($end !== false){
+            if($end >= $cur_pos //not ended
+                && $end <= $cur_pos+$length
+            ){
+                //cut end
+                $result = $result |  self::SUBST_END;
+
+                if($end < $cur_pos+$length){
+
+                    if($node->nodeType == XML_TEXT_NODE){
+                        $node_text = trim($node->wholeText);
+                        if(!empty($node_text)){
+                            $node_text = mb_substr($node_text,0,($end - $cur_pos));
+                            //var_dump($node_text);
+                            $node->replaceData(0,$node->length,$node_text);
+                        }
+                    }
+                }
+                //stop traversing
+                return $result;
+            }
+        }
+
+        $cur_pos += $length;
+        $this->substring_pos = $cur_pos;
+
+        $remove = array();
+        if($node->hasChildNodes()){
+            $delete_all = false;
+            foreach($node->childNodes as $child){
+                //var_dump("process ".$child->getNodePath());
+                if($delete_all){
+                    $remove[] = $child;
+                    continue;
+                }
+
+                $child_result = $this->substringNode($child,$start,$end,$callbacks);
+                $outside_zone  = !($this->substring_pos > $start && $this->substring_pos < $end );
+
+                if($child_result & self::SUBST_PRESERVE){
+                    //ноду придется оставить и себя, как родителя
+                    $result = $result | self::SUBST_PRESERVE;
+                    continue;
+                }
+
+                if(($child_result == self::SUBST_NOMOD) && $outside_zone){
+                    // удаляем дочернюю ноду, как ненужную
+                    $remove[] = $child;
+                    continue;
+                }
+                if($child_result & self::SUBST_START){
+                    $result = $result |  self::SUBST_START;
+                }
+
+                if($child_result & self::SUBST_END){
+                    $result = $result |  self::SUBST_END;
+                    $delete_all = true;
+                }
+            }
+        }
+
+        foreach($remove as $child){
+            $node->removeChild($child);
+        }
+
+        return $result;
+    }
+
+    /**
+     * Просчитывает длину текста ноды (не рекурсивно)
+     *
+     * @return void
+     * @author skoryukin
+     **/
+    protected function strlenNode($node)
+    {
+        $result = 0;
+
+        if($node->nodeType == XML_ELEMENT_NODE ){
+            //$result = mb_strlen($node->nodeValue);
+            // count child nodes
+        }elseif($node->nodeType == XML_TEXT_NODE){
+            //may be use isWhitespaceInElementContent
+            //var_dump($node->wholeText);
+            $node_text = trim($node->wholeText);
+            if(!empty($node_text)){
+                $result = mb_strlen($node_text);
+            }
+            //var_dump($node_text,$result);
+        }else{
+            //var_dump(get_class($node));
+            //var_dump($node->nodeType);
+        }
+
+
+        //var_dump("len",$result);
+        //DOMCdataSection and others are not counted
+        return $result;
+
+
+    }
+
+    public $doc_template = "<html><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" /><body>%s</body></html>";
+
+    protected function getDocument($html)
+    {
+        $html = sprintf($this->doc_template,$html);
+
+        $doc =  new DOMDocument("1.0","UTF-8");
+        $doc->preserveWhiteSpace = false;
+        $doc->substituteEntities = true;
+        $doc->resolveExternals = false;
+
+        @$doc->loadHTML($html);
+        $doc->normalizeDocument();
+
+        return $doc;
+    }
+
+    protected function getRootNode($doc)
+    {
+        $xpath = new DOMXPath($doc);
+        $roots = $xpath->query("/html/body");
+        $root = ($roots->item($roots->length -1));
+        return $root;
+    }
+
+    protected function getDocumentHTML($doc)
+    {
+        $result = "";
+        if(version_compare(PHP_VERSION, '5.3.6', '>=')){
+            $root =  $this->getRootNode($doc);
+            foreach($root->childNodes as $element){
+                $result .=  $doc->saveHTML($element);
+            }
+        }else{
+            $result = $doc->saveHTML();
+            if(preg_match("@(?:.*)<body>(.*)</body>(?:.*)@ims",$result,$match))
+            {
+                $result = $match[1];
+            }
+
+        }
+        return $result;
+    }
+}
