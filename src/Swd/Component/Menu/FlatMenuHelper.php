45afaece3b9cdee4911d36dd08ec9cc6617e2054
Sawered <skoryukin@medee.ru>
2013-09-19 16:54:42 +0400

    Added MenuHelper

---
+++ src/Swd/Component/Menu/FlatMenuHelper.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Swd\Component\Menu;
+
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use LogicException, ArrayIterator;
+
+class FlatMenuHelper extends ArrayIterator {
+
+    protected $router;
+
+    public function __construct($array,$flags = 0,UrlGeneratorInterface $router = null)
+    {
+        $this->router  = $router;
+
+        $defaults = array('selected'=>false);
+
+        foreach($array as $k => $item){
+            $item[$k] = array_merge($defaults,$item);
+        }
+        parent::__construct($array,$flags);
+    }
+
+    public function selectPath($path)
+    {
+        $paths = array();
+        foreach($this as $k => $item ){
+            $paths[$k] =  parse_url($item['url'],PHP_URL_PATH);
+        }
+
+        arsort($paths,SORT_STRING);
+        //var_dump($paths,$path);
+
+        foreach($paths as $k => $item_path){
+            if(strpos($path,$item_path) === 0){
+                $this[$k]['selected'] = true;
+                break;
+            }
+        }
+    }
+
+    public function addRoute($title,$route_name,$route_params = array(),$abs = false)
+    {
+
+        $url = $this->getRouter()->generate($route_name,$route_params,$abs);
+        parent::append(array('name'=>$title,'route'=>array('name'=>$route_name,'params'=>$route_params,'absolute'=>$abs),'url'=>$url,'selected'=>false));
+    }
+
+    public function addUrl($title,$url)
+    {
+        parent::append(array('name'=>$title,'url'=>$url,'selected' => false));
+    }
+
+    public function setRouter($router){
+        $this->router  = $router;
+    }
+
+
+    protected function getRouter()
+    {
+        if(is_null($this->router)){
+            throw new LogicException('No UrlGenerator set');
+        }
+        return $this->router;
+    }
+}
+
+
